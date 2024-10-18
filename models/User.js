03719d96228cdeedeee1cfac50e2971107a4f7ca
chandrakanth98 <90927147+chandrakanth98@users.noreply.github.com>
2024-10-18 18:06:47 -0500

    Login and Signup pages.

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    userType: {
+        type: String,
+        required: true,
+    },
+    specialization:{
+        type: String,
+    }
+});
+
+
+userSchema.pre('save', async function (next) {
+    try {
+        if (!this.isModified('password')) {
+            return next();
+        }
+        this.password = await bcrypt.hash(this.password, 10);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+// Define a method to compare passwords during login
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
