884c247bdb859f0beea4e42618c8f276c4634cae
sindhureddykandada <95462331+sindhureddykandada@users.noreply.github.com>
2024-10-18 20:36:47 -0500

    appointments file

---
+++ models/Appointment.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+}, { timestamps: true });
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+module.exports = Appointment;
